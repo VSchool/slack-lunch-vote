da9aa609bab724d803b0bc114916a5afddcc5d3c
Bob Ziroll <elderziroll@gmail.com>
2018-07-03 14:11:47 -0600

    Changed package.json start script

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const bodyParser = require("body-parser")
 const morgan = require("morgan")
 const mongoose = require("mongoose")
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.use(morgan("dev"))
 app.use(bodyParser.json())
